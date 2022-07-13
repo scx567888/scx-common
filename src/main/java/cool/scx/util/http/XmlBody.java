96621627478b4b41f13ed83bd30d9fb42acc9cdf
scx567888 <scx567888@outlook.com>
2022-07-13 16:22:26 +0800

    0.0.1 (#3)

--- src/main/java/cool/scx/util/http/XmlBody.java
+++ src/main/java/cool/scx/util/http/XmlBody.java
@@ -6,6 +6,9 @@ import cool.scx.util.ObjectUtils;
 import java.net.http.HttpRequest;
 import java.nio.charset.StandardCharsets;
 
+import static io.netty5.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
+import static io.netty5.handler.codec.http.HttpHeaderValues.APPLICATION_XML;
+
 /**
  * a
  */
@@ -25,7 +28,7 @@ public final class XmlBody implements Body {
 
     @Override
     public HttpRequest.BodyPublisher getBodyPublisher(HttpRequest.Builder builder) {
-        builder.setHeader("content-cool.scx.type", "application/xml;charset=utf-8");
+        builder.setHeader(CONTENT_TYPE.toString(), APPLICATION_XML + "; charset=utf-8");
         return HttpRequest.BodyPublishers.ofByteArray(bodyStr.getBytes(StandardCharsets.UTF_8));
     }
 
